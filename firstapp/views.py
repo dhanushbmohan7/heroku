179124c702e582e93dec6e7bc2f78f3560ddfe26
dhanush b mohan <dhanushmohan57@gmail.com>
2020-11-05 10:52:58 +0530

    new page added

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def second(request):
+    return render(request,'second.html')    

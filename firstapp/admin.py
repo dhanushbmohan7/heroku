126c1bfe3ffaa5698ad14905e27edaa7ebf0ea30
dhanush b mohan <dhanushmohan57@gmail.com>
2020-11-05 20:27:57 +0530

    navbar added

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Demo
 # Register your models here.
+class people(admin.ModelAdmin):
+    list_display=('name','age','email')
+
+
+admin.site.register(Demo,people)    
--- firstapp/models.py
+++ firstapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Demo(models.Model):
+    name=models.CharField(max_length=20)
+    age=models.IntegerField()
+    email=models.EmailField(max_length=50)

7d83739d8eabfde3d5a6880e9fa6ce9c0ec9045d
dhanush b mohan <dhanushmohan57@gmail.com>
2020-11-09 11:38:30 +0530

    fb and insta links added for 2

--- firstapp/models.py
+++ firstapp/models.py
@@ -6,3 +6,5 @@ class Demo(models.Model):
     age=models.IntegerField()
     email=models.EmailField(max_length=50)
     image1=models.ImageField(blank=True,null=True,upload_to='media/')
+    facebook_link=models.CharField(max_length=300,null=True,blank=True)
+    insta_link=models.CharField(max_length=300,null=True,blank=True)

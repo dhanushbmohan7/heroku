40cc79992c26884045a21c4326a10bf99273537b
Dhanush b Mohan <65810261+dhanushbmohan7@users.noreply.github.com>
2021-06-05 11:17:29 +0530

    Update admin.py
    user added admin

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Demo
+from .models import Demo,users
 # Register your models here.
 class people(admin.ModelAdmin):
     list_display=('name','age','email','image1')
+class USERS(admin.ModelAdmin):
+    list_display=['user']    
 
 
 admin.site.register(Demo,people)    
+admin.site.register(users,USERS)

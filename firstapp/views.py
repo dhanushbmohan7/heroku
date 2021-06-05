97bd7591a3bd81b9dee8574b7072f11eda3bc994
Dhanush b Mohan <65810261+dhanushbmohan7@users.noreply.github.com>
2021-06-05 11:13:36 +0530

    Update views.py
    added ip code

--- firstapp/views.py
+++ firstapp/views.py
@@ -7,7 +7,31 @@ from .models import Demo
                 
 def index(request):
     all=Demo.objects.all()
+    def get_ip(request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+
+            
+        return ip
+
+    ip=get_ip(request)
+    u=users(user=ip)
+    lst=[]
+    all_ip=users.objects.all()
+    for i in all_ip:
+        lst.append(i.user)
+    print(lst)    
     
+    if ip in lst:
+        pass
+    else:
+        u.save()
+    count=users.objects.all().count()
+
 
 
     return render(request,'index.html',{'all':all})
